e8cf263d13bdcc4d21eee9bd20d6d3d0548e8dd3
quentuez <dmitrij.klimenkoff7@gmail.com>
2021-03-04 22:36:37 +0700

    Edited .js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-console.warn("Hello");
+const bigInt = 12345678901234567890n;
+
+let str = "hello";
+
+let name = "Иван";
+alert( `Привет, ${name}!` );
+
+let isGreater = 4 > 1;
+alert( isGreater );
+
+let age = null;
+
+typeof undefined // "undefined"
+typeof 0 // "number"
+typeof 10n // "bigint"
+typeof true // "boolean"
+typeof "foo" // "string"
+typeof Symbol("id") // "symbol"
+typeof Math // "object" 
+typeof null // "object" 
+typeof alert // "function"
