5b0f1309883123af12df44050fa53df8dbb67c36
quentuez <dmitrij.klimenkoff7@gmail.com>
2021-03-07 10:59:11 +0700

    Edited .js files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,7 @@
 const bigInt = 12345678901234567890n;
 
-let str = "hello";
-
-let name = "Иван";
-alert( `Привет, ${name}!` );
-
 let isGreater = 4 > 1;
-alert( isGreater );
+console.log(isGreater);
 
 typeof undefined // "undefined"
 typeof 0 // "number"
@@ -18,8 +13,77 @@ typeof Math // "object"
 typeof null // "object" 
 typeof alert // "function"
 
-let age = prompt('Сколько тебе лет?', 100);
-alert(`Тебе ${age} лет!`);
+let name = prompt('Как тебя зовут?');
+alert(`Привет, ${name}!`);
 
-let isBoss = confirm("Ты здесь главный?");
-alert( isBoss );
+let value = true;
+value = String(value);
+console.log(typeof value);
+
+let str = "123";
+console.log(typeof str);
+let num = Number(str);
+console.log(typeof num);
+
+console.log(Boolean("0"));
+console.log(Boolean(0));
+
+console.log(8 % 3);
+console.log(2 ** 4);
+console.log(16 ** (1 / 4));
+
+let s = "my" + "string";
+console.log(s);
+
+console.log(6 - '2');
+console.log('6' / '2');
+
+
+let x = 1;
+console.log(+x);
+let y = -2;
+console.log(+y);
+console.log(+true);
+console.log(+"");
+
+let apples = "2";
+let oranges = "3";
+console.log(+apples + +oranges);
+
+let a = 1;
+let b = 2;
+
+let c = 3 - (a = b + 1);
+
+console.log(a);
+console.log(c);
+
+let d, e, f;
+
+d = e = f = 2 + 2;
+
+console.log(d);
+console.log(e);
+console.log(f);
+
+let n = 2;
+n += 5; // n = n + 5
+n *= 2; // n = n * 2
+console.log(n);
+
+let counter1 = 0;
+console.log(++counter1);
+let counter2 = 0;
+console.log(counter2++);
+
+
+/* for (a = 1, b = 3, c = a * b; a < 10; a++) {
+    ... 
+   } */
+
+console.log('А' == 'а');
+
+console.log('01' == 1);
+
+console.log(0 == false);
+console.log(0 === false);
