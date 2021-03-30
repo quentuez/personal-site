f90e8f234c0e19d8f9741d928f128e43f4089657
quentuez <dmitrij.klimenkoff7@gmail.com>
2021-03-30 21:29:44 +0700

    Edited project

---
+++ Main site/assets/js/main.js
@@ -0,0 +1,22 @@
+function getGuestName() {
+    let guestName = prompt("Введите ваше имя:");
+
+    if (guestName == '') {
+        guest.innerHTML = 'незнакомец';
+    } else if (guestName == null) {
+        guest.innerHTML = 'незнакомец';
+    } else if (guestName.length >= 15) {
+        guest.innerHTML = 'незнакомец с длинным именем';
+    } else {
+        guest.innerHTML = guestName;
+    }
+}
+
+getGuestName();
+
+let menuElem = document.getElementById('contentMenu');
+let titleElem = menuElem.querySelector('.title');
+
+titleElem.onclick = function () {
+    menuElem.classList.toggle('open');
+};
