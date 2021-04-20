4fd530f3fb9cb118f7f73d3229df4e8ae0f3cd4d
quentuez <dmitrij.klimenkoff7@gmail.com>
2021-04-20 17:51:22 +0700

    Edited code

---
+++ assets/form.php
@@ -0,0 +1,30 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$url = $_POST['url'];
+$message = $_POST['message'];
+
+$name = htmlspecialchars($name);
+$email = htmlspecialchars($email);
+$url = htmlspecialchars($url);
+$message = htmlspecialchars($message);
+
+$name = urldecode($name);
+$email = urldecode($email);
+$url = urldecode($url);
+$message = urldecode($message);
+
+$name = trim($name);
+$email = trim($email);
+$url = trim($url);
+$message = trim($message);
+
+echo $name;
+echo "<br>";
+echo $email;
+
+if (mail("dmitrij.klimenkoff7@gmail.com", "Заявка с сайта", "Имя:" . $name . ". E-mail: " . $email, "From: dmitrij.klimenkoff7@gmail.com \r\n")) {
+    echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}
