67bb7c277fb96196ff65ba4e54d5c8a177fbc6c3
quentuez <dmitrij.klimenkoff7@gmail.com>
2022-05-10 23:16:11 +0700

    Исправлены ошибки.

--- common.blocks/button/button.js
+++
@@ -1,7 +0,0 @@
-document
-  .querySelector(".intro-content__button")
-  .addEventListener("click", function () {
-    alert(
-      `Ширина окна: ${window.innerWidth}px, высота: ${window.innerHeight}px.`
-    );
-  });
--- common.blocks/map/map.js
+++ common.blocks/map/map.js
@@ -1,5 +1,4 @@
 DG.then(function () {
   let map = DG.map("map", { center: [54.98, 82.89], zoom: 13 });
-
   DG.marker([54.98, 82.89]).addTo(map);
 });
