267e577d3ed5b6d194fcf75a17f8625b0830237a
quentuez <dmitrij.klimenkoff7@gmail.com>
2022-12-12 21:33:12 +0700

    Исправлена архитектура сайта в соответствии с методологией БЭМ.

--- common.blocks/switcher/switcher.js
+++ common.blocks/switcher/switcher.js
@@ -1,15 +1,13 @@
-document.querySelector(".header__switcher").addEventListener("click", function () {
+document.querySelector(".switcher").addEventListener("click", function () {
   let ElementsWithLightTheme = document.querySelectorAll("[class*='theme_light']");
   if (this.checked) {
     for (let elem of ElementsWithLightTheme) {
       elem.className = elem.className.replace(/theme_light/, "theme_dark");
-      localStorage.setItem(elem.className, "theme_dark");
     }
   } else {
     let ElementsWithDarkTheme = document.querySelectorAll("[class*='theme_dark']");
     for (let elem of ElementsWithDarkTheme) {
       elem.className = elem.className.replace(/theme_dark/, "theme_light");
-      localStorage.setItem(elem.className, "theme_light");
     }
   }
 })
